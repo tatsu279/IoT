c6515c4887680c9eb120c8f8c12035dfb50ad839
Cuong Dinh <cuonglesinh1610@gmail.com>
2026-03-17 16:06:37 +0700

    Delete lab306 directory

--- lab306/src/main/java/com/dinhquoccuong/lab306/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dinhquoccuong.lab306.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.dinhquoccuong.lab306.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
