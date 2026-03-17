f98c6eaf7e40b983a43f8120925f39b9ea3b10dd
Cuong Dinh <cuonglesinh1610@gmail.com>
2026-03-17 16:10:18 +0700

    Delete lab306 directory

--- lab306/src/main/java/com/dinhquoccuong/lab306/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dinhquoccuong.lab306.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.dinhquoccuong.lab306.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
