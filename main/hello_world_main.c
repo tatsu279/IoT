21176431ed7fa74a470cd59c7a130a70bd632a2c
Cuong Dinh <cuonglesinh1610@gmail.com>
2026-03-17 16:16:46 +0700

    lab203

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
 
 #include <stdio.h>
 #include <inttypes.h>
@@ -7,10 +12,22 @@
 #include "esp_chip_info.h"
 #include "esp_flash.h"
 
+TaskHandle_t HelloWorldTaskHandle = NULL;
+void HelloWorld_Task(void *arg)
+{
+while (1)
+{
+printf("Task running: Hello World ..\n");
+vTaskDelay(1000 / portTICK_PERIOD_MS);
+}
+}
 void app_main(void)
 {
-printf("Hello world!\n");
-printf("Restarting now.\n");
-fflush(stdout);
-//esp_restart();
+    xTaskCreate(HelloWorld_Task, "HelloWorld", 4096, NULL, 10, &HelloWorldTaskHandle);
+    while (1)
+    {
+        printf("Task running: app main ..\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    //xTaskCreatePinnedToCore(HelloWorld_Task, "HelloWorld", 4096, NULL, 10, &HelloWorldTaskHandle, 1); // Run on Core 1
 }
