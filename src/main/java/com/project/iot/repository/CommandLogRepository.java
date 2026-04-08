2570b2a78562121160ad5936cd00b7c5a7dfac2e
tatsu279 <cuonglesinh1610@gmail.com>
2026-04-08 10:56:26 +0700

    Fix DioException 500

--- src/main/java/com/project/iot/repository/CommandLogRepository.java
+++ src/main/java/com/project/iot/repository/CommandLogRepository.java
@@ -3,6 +3,7 @@ package com.project.iot.repository;
 import com.project.iot.entity.CommandLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,5 +12,7 @@ public interface CommandLogRepository extends JpaRepository<CommandLog, Long> {
     List<CommandLog> findTop10ByDeviceIdOrderBySentAtDesc(String deviceId);
     long countByDeviceId(String deviceId);
     CommandLog findFirstByDeviceIdOrderBySentAtAsc(String deviceId);
+    
+    @Transactional
     void deleteByDeviceId(String deviceId);
 }
